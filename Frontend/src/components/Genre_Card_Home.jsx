64dbf7a480a4573c5b16bf3de6243d04facf6013
dhirubhai-123 <dhirajkhedkar123@gmail.com>
2025-02-14 23:53:19 +0530

    Minor typo fixed in BasicHomePage

--- Frontend/src/components/Genre_Card_Home.jsx
+++ Frontend/src/components/Genre_Card_Home.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Genre_Card_Home = ({ genrePoster, movieName, movieId, onClick }) => {
+const Genre_Card_Home = ({ genrePoster, movieName, movieId, handleCardClick, onClick }) => {
 
+    const navigate = useNavigate();
 
     return (
         <div className="card glass min-w-60 sm:w-80 lg:w-100  hover:shadow-slate-700 hover:shadow-lg hover:scale-105 hover:cursor-pointer"
-            onClick={onClick}
         >
-            <figure className="relative">
+            <figure className="relative" onClick={onClick}>
                 <img
                     src={genrePoster ? genrePoster : 'https://res.cloudinary.com/dzsvbfzti/image/upload/v1738873325/MoviesBaros%20Images/dzqml1dvqybfrj6ua6pm.jpg'}
                     alt={movieName}
@@ -18,7 +19,7 @@ const Genre_Card_Home = ({ genrePoster, movieName, movieId, onClick }) => {
                 {/* <h2 className="card-title text-center">{genreTitle}</h2> */}
 
                 <div className="overflow-hidden">
-                    <button className="btn btn-primary w-full mt-4 truncate hover:scale-110 ">
+                    <button className="btn btn-primary w-full mt-4 truncate hover:scale-110 " >
                         {movieName ? movieName : "Watch All"}
                     </button>
                 </div>
