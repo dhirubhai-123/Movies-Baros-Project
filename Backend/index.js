b1d7f5ab314f6a76b829b3b4827acca6c8b6f970
dhirubhai-123 <dhirajkhedkar123@gmail.com>
2025-03-01 00:22:30 +0530

    new Deployed frontend url updated

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://movies-baros-project-frontend.onrender.com',
         credentials: true
     }
 ));
