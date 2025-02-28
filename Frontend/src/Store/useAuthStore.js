7ee395392f25b0898cf8e894f08439966dc56bc0
dhirubhai-123 <dhirajkhedkar123@gmail.com>
2025-03-01 00:44:36 +0530

    Update useAuthStore.js
    updated backend url with https://movies-baros-project-backend.onrender.com

--- Frontend/src/Store/useAuthStore.js
+++ Frontend/src/Store/useAuthStore.js
@@ -3,7 +3,7 @@ import { persist, createJSONStorage } from "zustand/middleware"
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://movies-baros-backend.onrender.com/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://movies-baros-project-backend.onrender.com/";
 
 
 export const useAuthStore = create(
